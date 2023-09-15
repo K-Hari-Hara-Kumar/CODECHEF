45e17d7a0283c29cfb732dca6b0214eb9579fe3b
K Hari Hara Kumar <khariharakumar3@gmail.com>
2023-09-15 14:49:14 +0530

    CODE CHEF PRACTICE PROBLEM: Sorting in python
    Difficulty Rating: 667
    
    Submission ID: 1020823126
    
    Simple Python Program on sorting
    used method : arr.sort()

---
+++ Turbo_Sort.py
@@ -0,0 +1,21 @@
+N = int(input())
+l1 = []
+for i in range(N):
+    ele = int(input())
+    l1.append(ele)
+l1.sort()
+for j in l1:
+    print(j)
+
+
+
+"""
+N --> no. of elements 
+l1  is an empty list
+asks elements and appends into l1
+
+l1.sort() -- > to sort in acesnding order in l1
+loop the l1 and print the elements 
+
+
+"""
