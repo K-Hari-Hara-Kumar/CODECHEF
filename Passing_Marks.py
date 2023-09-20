98db5e3965db20ed69580cda07046b15c8dfa213
K Hari Hara Kumar <khariharakumar3@gmail.com>
2023-09-20 11:46:02 +0530

    CodeChef Practice problem : Sorting using python
    Difficulty Rating: 855
    
    Submission ID : 1021628498
    
    Updated code with 100% positive results and satisfying the previous
    failed corner testcases and cases with boundary values, comparing to
    previous code it have less complexity and giving positive outcome
    Explaination is given in the code.

--- Passing_Marks.py
+++ Passing_Marks.py
@@ -1,16 +1,44 @@
-T = int(input())
-for i in range(T):
-    nx = input()
-    n1 = input()
-    l1 = nx.split(" ")
-    n = int(l1[0])
-    x = int(l1[1])
-    l3 = []
-    l2 = n1.split(" ")
-    for i in l2:
-        l3.append(int(i))
-    l3.sort()
-    for i in range(x):
-        l3 = l3[:-1]
-        num1 = l3[-1]
-    print(num1-1)
+for i in range(int(input())):
+    NX = list(map(int,input().split()))
+    N=NX[0]
+    X = NX[1]
+    marks = list(map(int,input().split()))
+    marks.sort()
+    for i in range(X):
+        temp = marks.pop()
+    print(temp-1)
+
+
+"""
+Sample Testcaes:
+
+Input:
+
+3
+2 2
+5 1
+4 1
+5 1 7 4
+4 3
+15 70 100 31
+
+output:
+
+0
+6
+30
+
+Explaination:
+
+    1. Initial Loop indicates the testcase iteration.
+    2. NX is a list having number of students and Pass mark students
+    3. N --> no of students
+    4. X --> NO of students passed
+    5.marks --> list of marks of N students
+    6.sorted the marks list eg: [15,70,100,31] --> [15,31,70,100]
+    7.Based on the given probelm condition 2nd loop is used with parameters of X
+    8. it pops the max element from the marks list and given last stored elementin temp
+    9.printing temp -1 , required result
+
+
+"""
